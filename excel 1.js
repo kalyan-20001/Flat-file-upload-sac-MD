facb25d2736100472dbcc2d71ab568188fe8df40
kalyan-20001 <pavankalyanchalla120@gmail.com>
2026-01-22 17:02:25 +0530

    Update excel 1.js

--- excel 1.js
+++ excel 1.js
@@ -135,7 +135,7 @@
         onCustomWidgetAfterUpdate(changedProperties) {
             var that = this;
 
-            let xlsxjs = "https://priyankayala.github.io/ExcelUpload/xlsx.js";
+            let xlsxjs = "https://kalyan-20001.github.io/Flat-file-upload-sac-MD/xlsx 1.js";
             async function LoadLibs() {
                 try {
                     await loadScript(xlsxjs, _shadowRoot);
